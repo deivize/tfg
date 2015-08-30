3b0eb496ca34f5f77e78cf3c8571e91f9666a993
deivize <davidperezgarcia93@gmail.com>
2015-08-30 18:32:33 +0200

    dto para mostrar las localizaciones actuales

---
+++ tfg/src/main/java/es/udc/fi/tfg/dtos/ActivoLocalizacionDto.java
@@ -0,0 +1,48 @@
+package es.udc.fi.tfg.dtos;
+
+public class ActivoLocalizacionDto {
+	
+	private String nombreActivo;
+	
+	private double coord_x;
+	
+	private double coord_y;
+	
+	private double coord_z;
+
+	public String getNombreActivo() {
+		return nombreActivo;
+	}
+
+	public void setNombreActivo(String nombreActivo) {
+		this.nombreActivo = nombreActivo;
+	}
+
+	public double getCoord_x() {
+		return coord_x;
+	}
+
+	public void setCoord_x(double coord_x) {
+		this.coord_x = coord_x;
+	}
+
+	public double getCoord_y() {
+		return coord_y;
+	}
+
+	public void setCoord_y(double coord_y) {
+		this.coord_y = coord_y;
+	}
+
+	public double getCoord_z() {
+		return coord_z;
+	}
+
+	public void setCoord_z(double coord_z) {
+		this.coord_z = coord_z;
+	}
+	
+	
+
+
+}
